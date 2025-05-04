6f96b1b0b96e69619fab5e76177f2de15546b3b3
gpain1999 <guillaume.pain9@gmail.com>
2025-05-04 14:56:51 +0200

    maj

--- update_valeurs.py
+++ update_valeurs.py
@@ -3,8 +3,8 @@ from supabase import create_client
 import fonctions.fonctions_api as f
 
 # On lit directement les variables d'environnement injectées par GitHub
-from dotenv import load_dotenv
-load_dotenv()  
+# from dotenv import load_dotenv
+# load_dotenv()  
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 season = os.getenv("SEASON")
